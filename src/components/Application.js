2c0b75f9217cd926ca4d8cb157d8ba06f143f256
SydannyOpal <sydanny15@gmail.com>
2022-06-16 19:22:25 -0400

    get app running properly

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,6 +5,18 @@ import "components/Application.scss";
 export default function Application(props) {
   return (
     <main className="layout">
+      <img
+        className="sidebar--centered"
+        src="images/logo.png"
+        alt="Interview Scheduler"
+      />
+      <hr className="sidebar__separator sidebar--centered" />
+      <nav className="sidebar__menu"></nav>
+      <img
+        className="sidebar__lhl sidebar--centered"
+        src="images/lhl.png"
+        alt="Lighthouse Labs"
+      />
       <section className="sidebar">
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
