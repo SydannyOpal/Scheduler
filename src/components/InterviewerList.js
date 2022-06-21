c21730e13610a5ec1bbab515b43b561c3a8e9cf7
SydannyOpal <sydanny15@gmail.com>
2022-06-21 09:29:54 -0400

    implement InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import InterviewerListItem from "components/InterviewerListItem";
+import "components/InterviewerList.scss";
+
+// const interviewers = [
+//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+// ];
+
+export default function InterviewerList(props) {
+
+  return (
+    <section className="props.interviewers">
+
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+      {props.interviewers.map((interviewer) => {
+        return ( <InterviewerListItem 
+          key={interviewer.id}
+          name={interviewer.name}
+          avatar={interviewer.avatar} 
+          selected={interviewer.id === props.value}
+          setInterviewer={() => props.onChange(interviewer.id)}
+        />)
+      })}
+      </ul>
+    </section>
+  )
+
+}
+
