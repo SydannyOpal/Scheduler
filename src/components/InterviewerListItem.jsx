93508c88a1abb0fe25a36e44f9e22a9701f9c6ef
SydannyOpal <sydanny15@gmail.com>
2022-06-21 09:32:11 -0400

    implement InterviewerListItem component

---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,35 @@
+import classNames from "classnames";
+import React, { Component } from "react";
+import "components/InterviewerListItem.scss";
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+export default function InterviewListItem(props) {
+  const interviewListItemClass = classNames('interviewers__item',{
+    'interviewers__item--selected': props.selected
+  })
+
+  return(
+    <li 
+      className={interviewListItemClass} 
+      onClick={props.setInterviewer}>
+
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+     {props.selected ? props.name : undefined}
+    </li>
+  )
+}
+
+// id:number - the id of the interviewer
+// name:string - the name of the interviewer
+// avatar:url - a url to an image of the interviewer
+// selected:boolean - determines if an interviewer is selected or not and displays the name and applies appropriate styles if selected.
+// setInterviewer:function - is run when the <InterviewerListItem> is clicked. This function receives the interviewer's id as an argument. It sets the selected interviewer.
