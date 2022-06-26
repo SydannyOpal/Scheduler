6c895cef28fbee2e902d99229a7f37bcd7eba424
SydannyOpal <sydanny15@gmail.com>
2022-06-26 15:56:32 -0400

    implement validation feature

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "components/Appointment/styles.scss"
+import "components/Appointment/styles.scss";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
@@ -11,35 +11,65 @@ export default function Form({
   onCancel,
 }) {
   const [studentName, setStudentName] = useState(defaultStudentName || "");
-  const [interviewerId, setInterviewerId] = useState(defaultInterviewerId || null);
+  const [interviewerId, setInterviewerId] = useState(
+    defaultInterviewerId || null
+  );
+  const [interviewerNameError, setInterviewerNameError] = useState("");
+  const [studentNameError, setStudentNameError] = useState("");
 
   const reset = () => {
     setInterviewerId(null);
     setStudentName("");
-  }
+  };
 
   const cancel = () => {
     reset();
     onCancel();
-  }
+  };
+
+  const validate = () => {
+    if (!studentName) {
+      setStudentNameError("Student name cannot be blank");
+      return;
+    }
+    setStudentNameError("");
+
+    if (!interviewerId) {
+      setInterviewerNameError("Please select an interviewer");
+      return;
+    }
+
+    setInterviewerNameError("");
+    onSave(studentName, interviewerId);
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
           <input
+            data-testid="student-name-input"
             className="appointment__create-input text--semi-bold"
             value={studentName}
             type="text"
             placeholder="Enter Student Name"
-            onChange={event => setStudentName(event.target.value)}
+            onChange={(event) => setStudentName(event.target.value)}
           />
         </form>
-        <InterviewerList 
+
+        <section className="appointment__validation">
+          {studentNameError}
+        </section>
+
+        <InterviewerList
           value={interviewerId}
           onChange={setInterviewerId}
           interviewers={interviewers}
         />
+
+        <section className="appointment__validation">
+          {interviewerNameError}
+        </section>
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
